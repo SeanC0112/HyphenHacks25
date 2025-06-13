26453f17b1a29c976a171ee3cad7adb6e5211696
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2025-06-13 12:31:23 -0700

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from 'react';
 import Background from './background';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <Background />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
     </div>
   );
 }
