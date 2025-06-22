8f48cdcd7abea759f84b7e8a957e5131d32e4f13
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2025-06-22 15:16:49 -0700

    faqs

--- frontend/src/faqs.js
+++ frontend/src/faqs.js
@@ -32,8 +32,24 @@ function FAQItem({ question, answer }) {
 
 const faqData = [
     {
-        question: "example question 1?",
-        answer: "example answer 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        question: "Can I join without a team?",
+        answer: "Yes, you can join without a team! You can enter solo or find a team on discord or during designated team formation time at the event."
+    },
+    {
+        question: "Is food provided?",
+        answer: "Absolutely, lots of yummy food and snacks will be available for competitors!"
+    },
+    {
+        question: "I'm a have little or no coding expierence, can I participate?",
+        answer: "Of course! We will offer informative workshops for you to learn about coding! Alternatively, many skills besides coding are also important, like visual design."
+    },
+    {
+        question: "Is Hyphen-Hacks free?",
+        answer: "Yes, Hyphen-Hacks is free to everyone, in alignment with our mission of inclusivity!"
+    },
+    {
+        question: "Is there public transportation nearby?",
+        answer: "Just across the way for Lick-Wilmerding is Balboa Park BART Station, allowing for easy access for people all over the Bay Area! In addition, The K muni line runs in front of the school and the 8, 29, 49, 14, 54, and 43 bus lines have stops just outside."
     }
 ];
 
