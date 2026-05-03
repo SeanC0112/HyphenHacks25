b25ff4ab1e0e3a9b14aee096e27ad303d346ba5d
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-05-03 12:33:05 -0700

    clean uunused import

--- frontend/src/judges.js
+++ frontend/src/judges.js
@@ -1,61 +1,65 @@
-import React, {useRef} from "react";
-import './judges.css';
+import React from "react";
+import "./judges.css";
 
-const JudgeItem = ({item}) => {
-    return (
-        <div className="judge-item">
-            <img src={item.image} alt={item.name} className="judge-image" />
-            <h3 className="judge-name">{item.name}</h3>
-            <p className="judge-bio">{item.bio}</p>
-            <a
-                href={item.linkedin}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <img src="/linkedin.png"  className="judge-linkedin" />
-            </a>
-        </div>
-    );
-}
+const JudgeItem = ({ item }) => {
+  return (
+    <div className="judge-item">
+      <img src={item.image} alt={item.name} className="judge-image" />
+      <h3 className="judge-name">{item.name}</h3>
+      <p className="judge-bio">{item.bio}</p>
+      <a href={item.linkedin} target="_blank" rel="noopener noreferrer">
+        <img src="/linkedin.png" className="judge-linkedin" />
+      </a>
+    </div>
+  );
+};
 
 const JudgesData = [
-    {
-        name: "Thijs Simonian",
-        image: "/thijs.jpg",
-        bio: "Building video models at LMArena. Previous SWE at Discord. LWHS Class of '24!",
-        linkedin: "https://www.linkedin.com/in/thijsdev/"
-    },
-    // {
-    //     name: "Brenda Ng",
-    //     image: "/brenda.jpg",
-    //     bio: "Applied AI/ML Exec Director at JPMorganChase. Works with LLMs + knowledge graphs for fintech recommendations. Passionate about responsible generative AI.",
-    //     linkedin: "https://www.linkedin.com/in/brenda-ng/"
-    // }, 
-    {
-        name: "Nena Bains,",
-        image: "/nena.jpg",
-        bio: "Patent attorney working with start ups for the last 25 years, including new technoloiges from UCSF and Stanford incubators.  Technical experience in all things mechancial, electrical, and software/AI as it relates to the human body and a deep bench in financing diligence.",
-        linkedin: "https://www.linkedin.com/in/nena-bains-jd-90879/"
-    },
-    {
-        name: "Shane Lee",
-        image: "/shane.png",
-        bio: "AI/ML Engineer working in the financial analytics space building risk models for mortgage loans.",
-    }
+  {
+    name: "Thijs Simonian",
+    image: "/thijs.jpg",
+    bio: "Building video models at LMArena. Previous SWE at Discord. LWHS Class of '24!",
+    linkedin: "https://www.linkedin.com/in/thijsdev/",
+  },
+  // {
+  //     name: "Brenda Ng",
+  //     image: "/brenda.jpg",
+  //     bio: "Applied AI/ML Exec Director at JPMorganChase. Works with LLMs + knowledge graphs for fintech recommendations. Passionate about responsible generative AI.",
+  //     linkedin: "https://www.linkedin.com/in/brenda-ng/"
+  // },
+  {
+    name: "Nena Bains,",
+    image: "/nena.jpg",
+    bio: "Patent attorney working with start ups for the last 25 years, including new technoloiges from UCSF and Stanford incubators.  Technical experience in all things mechancial, electrical, and software/AI as it relates to the human body and a deep bench in financing diligence.",
+    linkedin: "https://www.linkedin.com/in/nena-bains-jd-90879/",
+  },
+  {
+    name: "Shane Lee",
+    image: "/shane.png",
+    bio: "AI/ML Engineer working in the financial analytics space building risk models for mortgage loans.",
+  },
 ];
 
 const Judges = React.forwardRef((props, ref) => (
-    <div className="judges" ref={ref}>
-        <h1 className="judges-title">Judges</h1>
-        <div className="judges-wrapper">
-            {JudgesData.map((item, index) => (
-                <JudgeItem key={index} item={item} />
-            ))}
-        </div>
-        <div className="judges-footer">
-            <p className="judges-footer-text">Interested in being a judge? <a href="https://forms.gle/AXwpXXfqNx3XZPjx6" target="_blank" rel="noopener noreferrer">Sign up here!</a></p>
-        </div>
+  <div className="judges" ref={ref}>
+    <h1 className="judges-title">Judges</h1>
+    <div className="judges-wrapper">
+      {JudgesData.map((item, index) => (
+        <JudgeItem key={index} item={item} />
+      ))}
     </div>
-))
+    <div className="judges-footer">
+      <p className="judges-footer-text">
+        Interested in being a judge?{" "}
+        <a
+          href="https://forms.gle/AXwpXXfqNx3XZPjx6"
+          target="_blank"
+          rel="noopener noreferrer">
+          Sign up here!
+        </a>
+      </p>
+    </div>
+  </div>
+));
 
 export default Judges;
