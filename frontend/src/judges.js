a76ae46f10fde383f0a4579aae5f557a4f1c71db
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-05-19 21:00:43 -0700

    remove old judges

--- frontend/src/judges.js
+++ frontend/src/judges.js
@@ -22,22 +22,11 @@ const JudgesData = [
     linkedin: "https://www.linkedin.com/in/thijsdev/",
   },
   // {
-  //     name: "Brenda Ng",
-  //     image: "/brenda.jpg",
-  //     bio: "Applied AI/ML Exec Director at JPMorganChase. Works with LLMs + knowledge graphs for fintech recommendations. Passionate about responsible generative AI.",
-  //     linkedin: "https://www.linkedin.com/in/brenda-ng/"
+  //   name: "Nena Bains,",
+  //   image: "/nena.jpg",
+  //   bio: "Patent attorney working with start ups for the last 25 years, including new technoloiges from UCSF and Stanford incubators.  Technical experience in all things mechancial, electrical, and software/AI as it relates to the human body and a deep bench in financing diligence.",
+  //   linkedin: "https://www.linkedin.com/in/nena-bains-jd-90879/",
   // },
-  {
-    name: "Nena Bains,",
-    image: "/nena.jpg",
-    bio: "Patent attorney working with start ups for the last 25 years, including new technoloiges from UCSF and Stanford incubators.  Technical experience in all things mechancial, electrical, and software/AI as it relates to the human body and a deep bench in financing diligence.",
-    linkedin: "https://www.linkedin.com/in/nena-bains-jd-90879/",
-  },
-  {
-    name: "Shane Lee",
-    image: "/shane.png",
-    bio: "AI/ML Engineer working in the financial analytics space building risk models for mortgage loans.",
-  },
 ];
 
 const Judges = React.forwardRef((props, ref) => (
